c7d61df89c3bf712746315b3bc134ef816767f5a
aamrasilo <aamrasilo@protonmail.com>
2024-12-18 20:22:10 +0530

    dotfiles: user: neovim: Set keymaps to manage active buffers

--- user/neovim/config/lua/remaps.lua
+++ user/neovim/config/lua/remaps.lua
@@ -1,2 +1,7 @@
 -- Remap leader
 vim.g.mapleader = " "
+
+-- Manage buffers
+vim.keymap.set("n", "<C-Tab>", "<CMD>bn<CR>", { desc = "Switch to next buffer" })
+vim.keymap.set("n", "<C-S-Tab>", "<CMD>bp<CR>", { desc = "Switch to previous buffer" })
+vim.keymap.set("n", "<C-q>", "<CMD>bd<CR>", { desc = "Close current buffer" })
