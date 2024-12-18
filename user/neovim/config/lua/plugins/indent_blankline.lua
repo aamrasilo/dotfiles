cb51a18edb7eb86f03363dd0c12828c261e3a7f6
aamrasilo <aamrasilo@protonmail.com>
2024-12-18 18:23:43 +0530

    dotfiles: user: neovim: Enable indentation guides with lukas-reineke/indent-blankline.nvim

---
+++ user/neovim/config/lua/plugins/indent_blankline.lua
@@ -0,0 +1,10 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  tag = "v2.20.8",
+  event = "BufRead",
+  opts = {
+    char = "▎",
+    context_char = "▎",
+    show_current_context = true,
+  },
+}
